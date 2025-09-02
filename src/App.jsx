3ba6f6706c7e74c4d2c9cf2d33abc1f0bf9b3f57
Tali <talicabana@gmail.com>
2025-09-02 01:12:44 -0300

    Agregamos un spinner de pacman para mostrar mientras carga el personaje solicitado

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Personaje from "./components/Personaje";
 import logo from "./assets/Dragon_Ball_Z_logo.svg";
 import { Button } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import { PacmanLoader } from "react-spinners";
 
 function App() {
   // Guardo los datos en un estado
   const [dataPersonaje, setDatoPersonaje] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     consultaAPI();
@@ -15,29 +17,46 @@ function App() {
     try {
       // aqui van todas las lineas de código que quiero
       const respuesta = await fetch(
-        "https://dragonball-api.com/api/characters/"+personajeAleatorio());
+        "https://dragonball-api.com/api/characters/" + personajeAleatorio()
+      );
       console.log(respuesta);
       if (respuesta.status === 200) {
         const datos = await respuesta.json();
         console.log(datos);
-        setDatoPersonaje(datos)
+        setDatoPersonaje(datos);
+        setLoading(false);
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
-  const personajeAleatorio = ()=>{
-    return Math.floor(Math.random() * (40-1+1+1)+1);
-  }
+  const personajeAleatorio = () => {
+    return Math.floor(Math.random() * (40 - 1 + 1 + 1) + 1);
+  };
 
   // nombreProps valor que envío debe ser el nombre del state, va entre {}
+
+  // la lógica va entre llaves siempre
+  // ternario: tal cosa?si pasa:si no pasa
   return (
     <main className="container my-5 text-center">
       <img src={logo} alt="Logo de Dragon Ball Z" className="w-50 mb-4" />
-      <Personaje dataPersonaje={dataPersonaje}></Personaje> 
-      
-      <Button variant="danger" className="mt-4" onClick={consultaAPI}>
+      {loading ? (
+        <div className="d-flex justify-content-center my-3">
+          <PacmanLoader
+            color={"#fd6d25ff"}
+            loading={loading}
+            size={15}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          ></PacmanLoader>
+        </div>
+      ) : (
+        <Personaje dataPersonaje={dataPersonaje}></Personaje>
+      )}
+
+      <Button variant="danger" className="my-4" onClick={consultaAPI}>
         Obtener personaje
       </Button>
     </main>
